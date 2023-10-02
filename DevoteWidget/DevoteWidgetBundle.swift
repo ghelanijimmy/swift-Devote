88a9aeeec8f151347440a03184c0010f6473d06d
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-02 13:35:50 -0400

    static widget extension

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Jimmy Ghelani on 2023-10-02.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
